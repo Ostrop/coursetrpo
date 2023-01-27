5069aabbae3bdd578a08e753786b7812364514d0
Dikhnich Stepan <stepan.dihnich@mail.ru>
2023-01-28 00:52:54 +0300

    start registrpanel

--- CourseProjectTRPO/CourseProjectTRPO/RegistrationPanel.cs
+++ CourseProjectTRPO/CourseProjectTRPO/RegistrationPanel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,10 +14,44 @@ namespace CourseProjectTRPO
     public partial class RegistrationPanel : Form
     {
         checkUser user;
+        DataBase dataBase = new DataBase();
+        SqlDataAdapter adapter;
+        DataSet ds;
+        string sqlstring = string.Empty;
+        int selectedRow;
+
         public RegistrationPanel(checkUser user1)
         {
             InitializeComponent();
+            //изменение размера окна
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            StartPosition = FormStartPosition.CenterScreen;
             user = user1;
+            label2.Text = user.Surname + " " + user.Name + " " + user.Patronymic;
+        }
+
+        private void RegistrationPanel_Activated(object sender, EventArgs e)
+        {
+            label3.Text = DateTime.Today.ToShortDateString().ToString();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            sqlstring = "SELECT procedure_date AS 'Дата', naming AS 'Должность', surname AS 'Фамилия',  name AS 'Имя', patronymic AS 'Отчество', procedure_time AS 'Время', appear AS 'Явка' FROM Assigned_procedures INNER JOIN Staff ON Staff.id_staff = Assigned_procedures.id_staff INNER JOIN Posts ON Staff.id_post = Posts.id_post";
+            adapter = new SqlDataAdapter(sqlstring, dataBase.getConnection());
+            adapter.Fill(ds);
+            sqlstring = "SELECT examination_date AS 'Дата', naming AS 'Должность', surname AS 'Фамилия',  name AS 'Имя', patronymic AS 'Отчество',examination_time AS 'Время', appear AS 'Явка' FROM Examinations INNER JOIN Staff ON Staff.id_staff = Examinations.id_staff INNER JOIN Posts ON Staff.id_post = Posts.id_post";
+            adapter = new SqlDataAdapter(sqlstring, dataBase.getConnection());
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
